fb9820ccc1fb1128a123356fb7de1334136f240a
Nishant Gautam <gautamniishant@icloud.com>
2025-07-02 10:02:18 -0400

    Refactor(): Use CrewBase pattern

--- Multi Agent Systems/app/main.py
+++ Multi Agent Systems/app/main.py
@@ -1,60 +1,21 @@
-import os
-import yaml
-import json
-from crewai import Agent, Crew, Task, Process
-from dotenv import load_dotenv
-
-load_dotenv()
+from travel_planner_crew import TravelPlannerCrew
 
 
+# Instance of CrewBase
+travel_planner = TravelPlannerCrew()
 
-base_directory = os.path.dirname(os.path.abspath(__file__))
-
-
-agents_configuration_path = os.path.join(base_directory, "config", "agents.yaml")
-
-tasks_configuration_path = os.path.join(base_directory, "config", "tasks.yaml")
+# Crew method
+crew = travel_planner.travel_crew()
 
 
-with open(agents_configuration_path, 'r') as file:
-    agents_data = yaml.safe_load(file)
 
-
-with open(tasks_configuration_path, 'r') as file:
-    tasks_data = yaml.safe_load(file)
-
-
-researcher = Agent(
-    config= agents_data["researcher"]
-)
-
-planner = Agent(
-    config= agents_data["planner"]
-)
-
-research_task = Task(
-    config= tasks_data["research_task"],
-    agent=researcher
-)
-
-planning_task = Task(
-    config= tasks_data["planning_task"],
-    agent=planner,
-    context=[research_task]
-)
-
-crew = Crew(
-    agents=[researcher, planner],
-    tasks=[research_task, planning_task],
-    process=Process.sequential
-)
-
+# Input variables
 city = "Montreal"
 days = 6
 attractions_per_day = 3
 total_attractions = days * attractions_per_day
 
-
+# Dictionary of inputs
 inputs = {
     "city": city,
     "days": days,
@@ -62,7 +23,7 @@ inputs = {
     "total_attractions": total_attractions
 }
 
-
+# Run the crew
 result = crew.kickoff(inputs=inputs)
 
 print(result)
---
+++ Multi Agent Systems/app/travel_planner_crew.py
@@ -0,0 +1,73 @@
+import os
+import yaml
+from crewai import Agent, Task, Crew, Process
+from crewai.project import CrewBase, agent, crew, task
+from dotenv import load_dotenv
+
+load_dotenv()
+
+@CrewBase
+class TravelPlannerCrew:
+    """ A crew for planning a travel itinerary """
+
+    base_directory = os.path.dirname(os.path.abspath(__file__))
+
+    agents_configuration_path = os.path.join(base_directory, "config", "agents.yaml")
+
+    tasks_configuration_path = os.path.join(base_directory, "config", "tasks.yaml")
+
+
+    def __init__(self):
+
+        with open(self.agents_configuration_path, 'r') as file:
+            self.agents_data = yaml.safe_load(file)
+
+        with open(self.tasks_configuration_path, 'r') as file:
+            self.tasks_data = yaml.safe_load(file)
+
+    
+    @agent
+    def researcher(self) -> Agent:
+        """ A researcher agent that can research the internet for information """
+
+        return Agent(
+            config=self.agents_data["researcher"]
+        )
+    
+    @agent
+    def planner(self) -> Agent:
+        """ A planner agent that can plan the travel itinerary """
+
+        return Agent(
+            config=self.agents_data["planner"]
+        )
+    
+    @task
+    def research_task(self) -> Task:
+        """ A task for researching the internet for information about the travel itinerary """
+
+        return Task(
+            config=self.tasks_data["research_task"],
+            agent=self.researcher()
+        )
+    
+    @task
+    def planning_task(self) -> Task:
+        """ A task for planning the travel itinerary """
+
+        return Task(
+            config=self.tasks_data["planning_task"],
+            agent=self.planner()
+        )
+    
+    @crew
+    def travel_crew(self) -> Crew:
+        """ A crew for planning a travel itinerary """
+
+        return Crew(
+            agents = self.agents,
+            tasks = self.tasks,
+            process = Process.sequential
+        )
+    
+   
