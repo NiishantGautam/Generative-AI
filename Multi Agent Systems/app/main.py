91f6f6dd223cf5e06224d69ee5d80f4d7efdd5fe
Nishant Gautam <gautamniishant@icloud.com>
2025-07-01 22:59:13 -0400

    Add simple iteration of logic

--- Multi Agent Systems/app/main.py
+++ Multi Agent Systems/app/main.py
@@ -1,6 +1,7 @@
 import os
 import yaml
-from crewai import Agent, Crew, Task
+import json
+from crewai import Agent, Crew, Task, Process
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -22,3 +23,46 @@ with open(agents_configuration_path, 'r') as file:
 with open(tasks_configuration_path, 'r') as file:
     tasks_data = yaml.safe_load(file)
 
+
+researcher = Agent(
+    config= agents_data["researcher"]
+)
+
+planner = Agent(
+    config= agents_data["planner"]
+)
+
+research_task = Task(
+    config= tasks_data["research_task"],
+    agent=researcher
+)
+
+planning_task = Task(
+    config= tasks_data["planning_task"],
+    agent=planner,
+    context=[research_task]
+)
+
+crew = Crew(
+    agents=[researcher, planner],
+    tasks=[research_task, planning_task],
+    process=Process.sequential
+)
+
+city = "Montreal"
+days = 6
+attractions_per_day = 3
+total_attractions = days * attractions_per_day
+
+
+inputs = {
+    "city": city,
+    "days": days,
+    "attractions_per_day": attractions_per_day,
+    "total_attractions": total_attractions
+}
+
+
+result = crew.kickoff(inputs=inputs)
+
+print(result)
